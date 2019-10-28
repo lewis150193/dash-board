fb351a266a402f9ef8a0f86a5f5216886cef3a1f
Timo Triebensky <30630233+binsky08@users.noreply.github.com>
2019-10-28 21:11:16 +0100

    add time widget (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import React from 'react'
 import {ImageWidget} from "./components/image-widget";
 import {WeatherWidget} from "./components/weather-widget";
 import {TFLWidget} from "./components/london-transport-widget";
+import {TimeWidget} from "./components/time-widget";
 
 const App: React.FC = () => (
       <div className="container">
-          <h1>Interactive Dashboard</h1>
+          <h1 className="title">Interactive Dashboard</h1>
+          <div className="grid-item display-inline right-absolute"><TimeWidget /></div>
           <div className="grid-row">
             <div className="grid-item"><ImageWidget /></div>
             <div className="grid-item"><WeatherWidget /></div>
