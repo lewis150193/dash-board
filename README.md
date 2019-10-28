f71a077ba24fb456884ddb85146cf1878bcd7250
Kevin Rouwkema <kevinrouwkema@gmail.com>
2019-10-28 22:19:11 +0100

    Updated readme file, added normilze css & added styling, rebuild HTML (#5)

--- README.md
+++ README.md
@@ -42,4 +42,9 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
-# dash-board
+# Trouble shoot
+If you have any trouble running the project, be sure you have installed the following things:
+* Node
+* yarn
+* react-scripts
+* typescript 
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import './normalize.css';
 import './App.css';
 import './fonts/theimmortal.ttf'
 import React from 'react'
@@ -8,13 +9,17 @@ import {TimeWidget} from "./components/time-widget";
 
 const App: React.FC = () => (
       <div className="container">
+        <div className="header">
           <h1 className="title">Interactive Dashboard</h1>
-          <div className="grid-item display-inline right-absolute"><TimeWidget /></div>
+          <div className=""><TimeWidget /></div>
+        </div>
+        <div className="content">
           <div className="grid-row">
             <div className="grid-item"><ImageWidget /></div>
             <div className="grid-item"><WeatherWidget /></div>
             <div className="grid-item"><TFLWidget /></div>
           </div>
+        </div>
       </div>
 );
 
