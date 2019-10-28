9677aa01e609a3bd3cddf9439ffde3e8c80cc743
Lewis Williams <LewisWilliams@abcdeMacBook.local>
2019-10-28 20:58:39 +0000

    added custom text

--- src/components/time-widget.tsx
+++ src/components/time-widget.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// @ts-ignore
 import IntervalTimer from 'react-interval-timer';
 
 const TimeWidgetComponent = () => (
@@ -16,25 +15,25 @@ const TimeWidgetComponent = () => (
                 var second = today.getSeconds();
                 var prepand = (hour >= 12)? " PM ":" AM ";
                 hour = (hour >= 12)? hour - 12: hour;
-                if (hour===0 && prepand===' PM ') { 
-                    if (minute===0 && second===0) { 
+                if (hour===0 && prepand===' PM ') {
+                    if (minute===0 && second===0) {
                         hour=12;
                         prepand=' Noon';
-                    } else { 
+                    } else {
                         hour=12;
                         prepand=' PM';
-                    } 
-                } 
-                if (hour===0 && prepand===' AM ') { 
-                    if (minute===0 && second===0) { 
+                    }
+                }
+                if (hour===0 && prepand===' AM ') {
+                    if (minute===0 && second===0) {
                         hour=12;
                         prepand=' Midnight';
-                    } else { 
+                    } else {
                         hour=12;
                         prepand=' AM';
-                    } 
+                    }
                 }
-                
+
                 var timeObjects = document.querySelectorAll('#currentTime');
                 for(var i=0; i<timeObjects.length; i++){
                     timeObjects[i].innerHTML = daylist[day]+', '+hour+':'+minute+':'+second+' '+prepand;
