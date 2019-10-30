ee07000dcb7a645973325ca26735fbbf6e87a1cc
Natalia C <natalia.calt@gmail.com>
2019-10-30 06:13:38 -0600

    added button to get the next picture, and styled it (#7)

--- src/components/image-widget.tsx
+++ src/components/image-widget.tsx
@@ -1,26 +1,26 @@
-import React, {ReactNode, useEffect, useState} from 'react';
-import {getImages} from "../utils/axiosCalls";
-
+import React, { ReactNode, useEffect, useState } from "react";
+import { getImages } from "../utils/axiosCalls";
 
 export const ImageWidget = () => {
-    const [date, setDate] = useState([]);
+  const [date, setDate] = useState([]);
+  const [isNext, setIsNext] = useState(false);
 
-    const randomImage = (): ReactNode => {
-        const randomNumber = Math.floor(Math.random() * 20);
-        const randomImage = date[randomNumber];
-            return (
-                <img src={randomImage} alt="Character"/>
-                    )
-    };
+  const randomImage = (): ReactNode => {
+    const randomNumber = Math.floor(Math.random() * 20);
+    const randomImage = date[randomNumber];
+    return <img className="picture" src={randomImage} alt="Character" />;
+  };
 
-    useEffect(() => {
-        getImages().then((char: any) => setDate(char));
+  useEffect(() => {
+    getImages().then((char: any) => setDate(char));
+  }, [isNext]);
 
-    },[]);
-
-    return (
-      <div>
-          {randomImage()}
-      </div>
-    )
+  return (
+    <div className="image-widget">
+      {randomImage()}
+      <button className="next-image" onClick={() => setIsNext(!isNext)}>
+        Next image
+      </button>
+    </div>
+  );
 };
